1077213abdb7b4ffe629712938dc8d52f3f4e77f
Cassius Cives Muñoz <cass-cives@hotmail.com>
2021-02-01 17:34:35 -0300

    Improved readability

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector("button").addEventListener("click", searchInfoByName)
+
+
+let nameDisplay = document.querySelector("#name")
+let characterImage = document.querySelector("#characterImage")
+let statusDisplay = document.querySelector("#status")
+let specieDisplay = document.querySelector("#specie")
+let genderDisplay = document.querySelector("#gender")
+let originDisplay = document.querySelector("#origin")
+let locationDisplay = document.querySelector("#location")
+
+
 function searchInfoByName(){
-    let nameDisplay = document.querySelector("#name")
-    let characterImage = document.querySelector("#characterImage")
-    let statusDisplay = document.querySelector("#status")
-    let specieDisplay = document.querySelector("#specie")
-    let genderDisplay = document.querySelector("#gender")
-    let originDisplay = document.querySelector("#origin")
-    let locationDisplay = document.querySelector("#location")
     let name = document.querySelector("input").value
     let url = "https://rickandmortyapi.com/api/character/"+`?name=${name.toString()}`
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         nameDisplay.innerText = "Name: " + data.results[0].name
         characterImage.src = data.results[0].image
         statusDisplay.innerText = "Status: " + data.results[0].status
