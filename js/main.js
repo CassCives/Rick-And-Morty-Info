3c6a4e7b205c3ea1308316bc64b8d657c1f1f697
Cassius Cives Muñoz <cass-cives@hotmail.com>
2021-02-01 17:28:00 -0300

    Improved readability

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector("button").addEventListener("click", searchInfoByName)
 function searchInfoByName(){
+    let nameDisplay = document.querySelector("#name")
+    let characterImage = document.querySelector("#characterImage")
+    let statusDisplay = document.querySelector("#status")
+    let specieDisplay = document.querySelector("#specie")
+    let genderDisplay = document.querySelector("#gender")
+    let originDisplay = document.querySelector("#origin")
+    let locationDisplay = document.querySelector("#location")
     let name = document.querySelector("input").value
     let url = "https://rickandmortyapi.com/api/character/"+`?name=${name.toString()}`
     fetch(url)
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        document.querySelector("#name").innerText = "Name: " + data.results[0].name
-        document.querySelector("#characterImage").src = data.results[0].image
-        document.querySelector("#status").innerText = "Status: " + data.results[0].status
-        document.querySelector("#specie").innerText = "Specie: " + data.results[0].species
-        document.querySelector("#gender").innerText = "Gender: " + data.results[0].gender
-        document.querySelector("#origin").innerText = "Origin: " + data.results[0].origin.name
-        document.querySelector("#location").innerText = "Location: " + data.results[0].location.name
+        nameDisplay.innerText = "Name: " + data.results[0].name
+        characterImage.src = data.results[0].image
+        statusDisplay.innerText = "Status: " + data.results[0].status
+        specieDisplay.innerText = "Specie: " + data.results[0].species
+        genderDisplay.innerText = "Gender: " + data.results[0].gender
+        originDisplay.innerText = "Origin: " + data.results[0].origin.name
+        locationDisplay.innerText = "Location: " + data.results[0].location.name
     })
     .catch(err => {
         console.log(`error ${err}`)
